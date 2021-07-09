1e6009b57c5268c3f11ac9ca97d272b122e2b7e8
Sascha Taumann <staumann@live.com>
2021-07-09 14:37:39 +0200

    Implementing stuff

--- core/src/main/kotlin/com/home/tool/model/Bill.kt
+++ core/src/main/kotlin/com/home/tool/model/Bill.kt
@@ -10,6 +10,6 @@ data class Bill(
     @Id @Column(name = "id") var id: String = "",
     @Column(name = "payed_by") var payedBy: String = "",
     @Column(name = "shop_id") var shopId: String = "",
-    @Column(name = "discount") var discount: Double = 0.0,
+    @Column(name = "discount") var discount: Double? = 0.0,
     @Column(name = "billing_date") @JsonFormat(pattern="dd.MM.yyyy") @Temporal(TemporalType.DATE)var date: Calendar = Calendar.getInstance()
 )
